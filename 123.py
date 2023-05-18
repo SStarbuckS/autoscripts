cc1523acca7667afaaecd7f4bd00e246814b75ba
SStarbuckS <22103918+SStarbuckS@users.noreply.github.com>
2023-05-19 00:46:39 +0800

    简单的抢券脚本

--- 123.py
+++ 123.py
@@ -2,42 +2,27 @@ import requests
 import json
 import datetime
 import time
+import threading
 
-def send_post_request(url, payload, headers):
-    response = requests.post(url, headers=headers, data=payload)
+# 定义全局变量
+URL = ""
+
+PAYLOAD = {
+    
+}
+
+HEADERS = {
+    
+}
+
+def send_post_request():
+    response = requests.post(URL, headers=HEADERS, data=json.dumps(PAYLOAD))
     return response.text
 
-def run_script(request_count, delay_time):
-    url = ""
-
-    payload = json.dumps({
-      
-    })
-    headers = {
-      
-    }
-    
-    current_time = datetime.datetime.now()
-    scheduled_time = input("请输入脚本的执行时间（格式为HH:MM:SS）：")
-    
-    scheduled_datetime = datetime.datetime.combine(current_time.date(), datetime.datetime.strptime(scheduled_time, '%H:%M:%S').time())
-    time_diff = (scheduled_datetime - current_time).total_seconds()
-    
-    if time_diff > 0:
-        print(f"等待 {time_diff:.0f} 秒后开始执行脚本...")
-        while time_diff > 0:
-            minutes, seconds = divmod(int(time_diff), 60)
-            print(f"\r剩余时间：{minutes:02d}:{seconds:02d}", end="")
-            time.sleep(1)
-            current_time = datetime.datetime.now()
-            time_diff = (scheduled_datetime - current_time).total_seconds()
-        print("\n开始执行脚本...")
-    else:
-        print("指定的执行时间已过，立即开始执行脚本")
-    
+def send_requests(request_count, delay_time=0):
     for i in range(request_count):
         start_time = time.time()  # 记录请求开始时间
-        response_text = send_post_request(url, payload, headers)
+        response_text = send_post_request()
         end_time = time.time()  # 记录请求结束时间
         
         # 输出日志
@@ -47,9 +32,66 @@ def run_script(request_count, delay_time):
         
         time.sleep(delay_time / 1000)  # 延迟指定时间（毫秒转换为秒）
 
+def run_script():
+    mode = int(input("请选择模式（1 - 单线程模式，2 - 多线程模式）："))
+
+    if mode == 1:  # 单线程模式
+        request_count = int(input("请输入要发送的请求次数："))
+        delay_time = float(input("请输入每个请求之间的延迟时间（毫秒）："))
+
+        current_time = datetime.datetime.now()
+        scheduled_time = input("请输入脚本的执行时间（格式为HH:MM:SS）：")
+
+        scheduled_datetime = datetime.datetime.combine(current_time.date(), datetime.datetime.strptime(scheduled_time, '%H:%M:%S').time())
+        time_diff = (scheduled_datetime - current_time).total_seconds()
+
+        if time_diff > 0:
+            print(f"等待 {time_diff:.0f} 秒后开始执行脚本...")
+            while time_diff > 0:
+                minutes, seconds = divmod(int(time_diff), 60)
+                print(f"\r剩余时间：{minutes:02d}:{seconds:02d}", end="")
+                time.sleep(1)
+                current_time = datetime.datetime.now()
+                time_diff = (scheduled_datetime - current_time).total_seconds()
+            print("\n开始执行脚本...")
+        else:
+            print("指定的执行时间已过，立即开始执行脚本")
+
+        send_requests(request_count, delay_time)
+
+    elif mode == 2:  # 多线程模式
+        concurrent_count = int(input("请输入并发请求数："))
+
+        current_time = datetime.datetime.now()
+        scheduled_time = input("请输入脚本的执行时间（格式为HH:MM:SS）：")
+
+        scheduled_datetime = datetime.datetime.combine(current_time.date(), datetime.datetime.strptime(scheduled_time, '%H:%M:%S').time())
+        time_diff = (scheduled_datetime - current_time).total_seconds()
+
+        if time_diff > 0:
+            print(f"等待 {time_diff:.0f} 秒后开始执行脚本...")
+            while time_diff > 0:
+                minutes, seconds = divmod(int(time_diff), 60)
+                print(f"\r剩余时间：{minutes:02d}:{seconds:02d}", end="")
+                time.sleep(1)
+                current_time = datetime.datetime.now()
+                time_diff = (scheduled_datetime - current_time).total_seconds()
+            print("\n开始执行脚本...")
+        else:
+            print("指定的执行时间已过，立即开始执行脚本")
+
+        threads = []
+        for _ in range(concurrent_count):
+            thread = threading.Thread(target=send_requests, args=(1,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+    else:
+        print("无效的模式选择！")
+
 # 主程序入口
 if __name__ == "__main__":
-    request_count = int(input("请输入要发送的请求次数："))
-    delay_time = float(input("请输入每个请求之间的延迟时间（毫秒）："))
-    
-    run_script(request_count, delay_time)
+    run_script()
