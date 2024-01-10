b4a10ed95695a83ae55dab31737ea5d20235733b
SStarbuckS <22103918+SStarbuckS@users.noreply.github.com>
2024-01-10 18:01:45 +0800

    Update docker-entrypoint.sh

--- Docker_file/speedtest/docker-entrypoint.sh
+++ Docker_file/speedtest/docker-entrypoint.sh
@@ -14,14 +14,8 @@ while true; do
             url="https://www.speedtest.net/api/js/servers?engine=js&search=${location}"
             echo "查询节点信息：$url"
             result=$(wget -qO- $url)
-            # 输出表头
-            echo -e "ID\tName\t\t\t\t\tLocation\t\tCountry"
-            echo "======================================================================="
-            
-            # 格式化输出节点信息
-            echo "$result" | jq -r '.[] | "\(.id)\t\(.sponsor)\t\t\(.name)\t\t\(.country)"'
-            #echo -e "返回的测速节点信息：\n$result"
-            read -p "请输入测速节点ID，数字(123\22333\666666): " server_id  < /dev/tty  # 添加 < /dev/tty 清除输入缓冲
+            echo -e "返回的测速节点信息：\n$result"
+            read -p "请输入测速节点ID，数字(123\22333\666666): " server_id
             echo "开始指定测速节点 $server_id..."
             speedtest -s $server_id
             ;;
