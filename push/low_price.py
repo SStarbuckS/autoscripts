62807b60e6fcea24b704b3ab0caac6f144737111
SStarbuckS <22103918+SStarbuckS@users.noreply.github.com>
2024-02-02 02:21:15 +0800

    Update low_price.py

--- push/low_price.py
+++ push/low_price.py
@@ -39,29 +39,32 @@ def get_product_info(product_url, expected_price_str):
         # 提取需要的字段
         current_price = data_json.get("currentPrice")
         sp_name = data_json.get("spName")
+        lower_price = data_json.get("lowerPrice")
 
         # 打印提取的信息（中文）
         print("商品名称:", sp_name)
         print("商品链接:", product_url)
         print("当前价格:", current_price)
+        print("史低价格:", lower_price)
+        print("提醒价格:", expected_price_str)
 
         # 比较当前价格和预期价格
         if current_price and float(current_price) <= float(expected_price_str):
-            send_notification(sp_name, product_url, current_price, expected_price_str)
+            send_notification(sp_name, product_url, current_price, expected_price_str, lower_price)
         
         print("--------------------")
     else:
         print(f"获取 {product_url} 数据失败。状态码:", response.status_code)
         send('商品低价提醒', '出错了请检查！')
 
-def send_notification(sp_name, product_url, current_price, expected_price_str):
+def send_notification(sp_name, product_url, current_price, expected_price_str, lower_price):
         title = "商品低价提醒"
-        content = f"商品名称：{sp_name}\n商品链接：{product_url}\n当前价格：{current_price} 小于或等于预期价格 {expected_price_str}！"
+        content = f"商品名称：{sp_name}\n商品链接：{product_url}\n当前价格：{current_price} 小于或等于预期价格 {expected_price_str}！\n历史最低价格参考：{lower_price}"
         send(title, content)
 
 # 例子：多个商品链接和预期价格
 product_and_prices = [
-    {"url": "https://detail.tmall.com/item.htm?id=123", "expected_price": "299.00"},
+    {"url": "https://detail.tmall.com/item.htm?id=123", "expected_price": "100.00"},
     # 添加更多商品链接和预期价格...
 ]
 
